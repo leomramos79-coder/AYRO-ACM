6ff4f237a5312ed32f795af5937b55cbadf0f1cb
leomramos79-coder <leomramos79@gmail.com>
2026-09-16 14:05:41 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 window.SUPABASE_URL = "https://cgkxxghiwbdhcvbrsslm.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_4EBTHZaMV7WLBEP68Wkyig_TCmSLOhlL";
+window.SUPABASE_ANON_KEY = "sb_publishable_4EBTHZaMV7WLBEP68Wkyig_TCmSLOhl";
